66e02d1bb662c552a7c185aa67a7bba018873355
mdonthi <mdonthi@us.ibm.com>
2016-10-05 16:14:25 -0500

    first commit

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -35,7 +35,7 @@ func main() {
 }
 
 // Init resets all the things
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
